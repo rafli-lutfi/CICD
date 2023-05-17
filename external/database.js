34a6cda118fa8536ab4c94d23161a417cdff4ea0
rafli-lutfi <rafli.lutfi1818@gmail.com>
2023-05-17 20:26:23 +0700

    add port env in database config

--- external/database.js
+++ external/database.js
@@ -4,6 +4,7 @@ const {
   DB_NAME,
   DB_USERNAME,
   DB_PASSWORD,
+  DB_PORT = 5432,
   DB_DIALECT = 'postgres'
 } = process.env;
 
@@ -13,20 +14,23 @@ module.exports = {
     "database": DB_NAME,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "test": {
     "host": DB_HOST,
     "database": DB_NAME,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "production": {
     "host": DB_HOST,
     "database": DB_NAME,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
-    "dialect": DB_DIALECT
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   }
 };
